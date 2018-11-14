9a12d752ac930769842f367e9ab03fdecc0f54c8
Peter Ferreira <Peterferreira41@gmail.com>
2018-11-14 21:00:37 +0200

    homepage changes

--- js/main.js
+++ js/main.js
@@ -115,3 +115,20 @@ function showDivs(n, no) {
     }
     x[slideIndex[no] - 1].style.display = "block";
 }
+
+// About - readme button collapsible
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
